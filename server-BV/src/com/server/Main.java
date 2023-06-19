95cb785e6b9cf080fd8990272ea0164ccf2ef245
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2023-06-20 00:27:29 +0800

    1

--- server-BV/src/com/server/Main.java
+++ server-BV/src/com/server/Main.java
@@ -1,8 +1,11 @@
 package com.server;
 
+import java.awt.*;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, FontFormatException {
         new Access();
     }
 }
