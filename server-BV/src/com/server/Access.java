1b4e26150639428757423fb32684b5d00dcae255
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2023-06-20 17:10:12 +0800

    separate the create bill funcn\

--- server-BV/src/com/server/Access.java
+++ server-BV/src/com/server/Access.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class Access extends JFrame {
     static String[][] billData = new String[1000][4];
@@ -1120,82 +1121,7 @@ public class Access extends JFrame {
         float finalTotal = total;
         saveButton.addActionListener(e -> {
             if (!customerNameField.getText().equals("")) {
-                /***** File creation ******/
-                try {
-                    File myObj = new File("sales.txt");
-                    if (myObj.createNewFile()) {
-                        System.out.println("File created: " + myObj.getName());
-                    }
-                } catch (IOException exception) {
-                    JOptionPane.showMessageDialog(frame, "Unable to create sales file");
-                    exception.printStackTrace();
-                }
-                /***** writing in file *****/
-                try {
-                    LocalDateTime myObj = LocalDateTime.now();
-                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-                    String[] formatedDate = myObj.format(formatter).split(" ");
-
-                    FileWriter myWriter = new FileWriter("sales.txt", true);
-                    myWriter.write(
-                            (Functions.recordCount("sales.txt") + 1) + ","
-                                    + formatedDate[0] + ","
-                                    + formatedDate[1] + ","
-                                    + finalTotal + ","
-                                    + customerNameField.getText() + "\n"
-                    );
-                    myWriter.close();
-                    JOptionPane.showMessageDialog(frame, "Bill saved");
-                } catch (IOException exception) {
-                    JOptionPane.showMessageDialog(frame, "Error saving data");
-                    exception.printStackTrace();
-                }
-
-                /******* Updating function *******/
-                try {
-                    File inputFile = new File("products.txt");
-                    if (!inputFile.isFile()) {
-                        System.out.println("File does not exist");
-                        return;
-                    }
-                    File tempFile = new File(inputFile.getAbsolutePath() + ".tmp");
-                    BufferedReader br = new BufferedReader(new FileReader("products.txt"));
-                    PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
-                    String line = null;
-                    //Read from the original file and write to the new
-                    //unless content matches data to be removed.
-                    while ((line = br.readLine()) != null) {
-                        float tempProductQuantity = 0;
-                        String[] token = line.split(",");
-                        for (int i = 0; billData[i][0] != null; i++) {
-                            if (!billData[i][0].equals("")) {
-                                if (billData[i][0].equals(token[0])) {
-                                    tempProductQuantity = tempProductQuantity + productQuantity[i];
-                                }
-                            }
-                        }
-                        if (tempProductQuantity > 0) {
-                            pw.println(token[0] + "," + token[1] + "," + token[2] + "," + (Float.parseFloat(token[3]) - tempProductQuantity));
-                            pw.flush();
-                        } else {
-                            pw.println(line);
-                        }
-                    }
-                    pw.close();
-                    br.close();
-                    //Delete the original file
-                    if (!inputFile.delete()) {
-                        System.out.println("Could not delete file");
-                        return;
-                    }
-                    //Rename the new file to the filename the original file had.
-                    if (!tempFile.renameTo(inputFile))
-                        System.out.println("Could not rename file");
-                } catch (FileNotFoundException ex) {
-                    ex.printStackTrace();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
+                Functions.writeTxt(frame, (int) finalTotal,customerNameField,billData,productQuantity);
             } else if (customerNameField.getText().equals("")) {
                 JOptionPane.showMessageDialog(frame, "Please enter customer name");
             }
--- server-BV/src/com/server/Functions.java
+++ server-BV/src/com/server/Functions.java
@@ -1,7 +1,10 @@
 package com.server;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Functions {
@@ -87,5 +90,84 @@ public class Functions {
             exception.printStackTrace();
         }
         return searchedData;
+    }
+
+    public static void writeTxt(Frame frame, Integer finalTotal,JTextField customerNameField,String[][] billData,float[] productQuantity){
+        /***** File creation ******/
+        try {
+            File myObj = new File("sales.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            }
+        } catch (IOException exception) {
+            JOptionPane.showMessageDialog(frame, "Unable to create sales file");
+            exception.printStackTrace();
+        }
+        /***** writing in file *****/
+        try {
+            LocalDateTime myObj = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+            String[] formatedDate = myObj.format(formatter).split(" ");
+
+            FileWriter myWriter = new FileWriter("sales.txt", true);
+            myWriter.write(
+                    (Functions.recordCount("sales.txt") + 1) + ","
+                            + formatedDate[0] + ","
+                            + formatedDate[1] + ","
+                            + finalTotal + ","
+                            + customerNameField.getText() + "\n"
+            );
+            myWriter.close();
+            JOptionPane.showMessageDialog(frame, "Bill saved");
+        } catch (IOException exception) {
+            JOptionPane.showMessageDialog(frame, "Error saving data");
+            exception.printStackTrace();
+        }
+
+        /******* Updating function *******/
+        try {
+            File inputFile = new File("products.txt");
+            if (!inputFile.isFile()) {
+                System.out.println("File does not exist");
+                return;
+            }
+            File tempFile = new File(inputFile.getAbsolutePath() + ".tmp");
+            BufferedReader br = new BufferedReader(new FileReader("products.txt"));
+            PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
+            String line = null;
+            //Read from the original file and write to the new
+            //unless content matches data to be removed.
+            while ((line = br.readLine()) != null) {
+                float tempProductQuantity = 0;
+                String[] token = line.split(",");
+                for (int i = 0; billData[i][0] != null; i++) {
+                    if (!billData[i][0].equals("")) {
+                        if (billData[i][0].equals(token[0])) {
+                            tempProductQuantity = tempProductQuantity + productQuantity[i];
+                        }
+                    }
+                }
+                if (tempProductQuantity > 0) {
+                    pw.println(token[0] + "," + token[1] + "," + token[2] + "," + (Float.parseFloat(token[3]) - tempProductQuantity));
+                    pw.flush();
+                } else {
+                    pw.println(line);
+                }
+            }
+            pw.close();
+            br.close();
+            //Delete the original file
+            if (!inputFile.delete()) {
+                System.out.println("Could not delete file");
+                return;
+            }
+            //Rename the new file to the filename the original file had.
+            if (!tempFile.renameTo(inputFile))
+                System.out.println("Could not rename file");
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
