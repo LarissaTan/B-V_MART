bb3661515ee6855d796c4e11ab807538de8d2660
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2023-06-21 04:15:56 +0800

    成功利用socket进行连接了，不过只是初步尝试

--- client-BV/app/src/main/java/com/example/bv_mart/fragment/StoreCommentFragment.java
+++ client-BV/app/src/main/java/com/example/bv_mart/fragment/StoreCommentFragment.java
@@ -1,5 +1,8 @@
 package com.example.bv_mart.fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -10,6 +13,8 @@ import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -24,6 +29,11 @@ import com.example.bv_mart.util.DateUtill;
 import com.example.bv_mart.util.MySQLiteHelper;
 import com.example.bv_mart.util.ToastUtil;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -80,17 +90,49 @@ public class StoreCommentFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 messages = et_chat_message.getText().toString();
-                if (TextUtils.isEmpty(messages)){
+                if (TextUtils.isEmpty(messages)) {
                     ToastUtil.showShort("内容为空！");
-                }else {
-                    chatMessageBean = new ChatMessageBean(MyUserID,MyUserID+"",messages,MainActivity.username, DateUtill.getCurrentTime());
-                    MySQLiteHelper.getInstance(getActivity()).insertMessages(chatMessageBean);
-                    adapter.refreshMessages();
-                    rv_Chat.scrollToPosition(adapter.getItemCount()-1);
-                    ToastUtil.showShort("发送成功");
-                    et_chat_message.setText("");
+                } else {
+                    chatMessageBean = new ChatMessageBean(MyUserID, MyUserID + "", messages, MainActivity.username, DateUtill.getCurrentTime());
+
+                    new AsyncTask<Void, Void, String>() {
+                        @Override
+                        protected String doInBackground(Void... voids) {
+                            String response = null;
+                            try {
+                                Socket socket = new Socket("10.0.2.2", 12345); // 替换为服务器的IP地址和端口号
+
+                                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+
+                                writer.println(messages); // 发送消息到服务器
+
+                                response = reader.readLine(); // 读取服务器的响应
+                                System.out.println("服务器响应：" + response);
+
+                                reader.close();
+                                writer.close();
+                                socket.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            return response;
+                        }
+
+                        @Override
+                        protected void onPostExecute(String response) {
+                            // 在网络操作完成后的回调方法中更新UI或执行其他操作
+                            MySQLiteHelper.getInstance(getActivity()).insertMessages(chatMessageBean);
+                            adapter.refreshMessages();
+                            rv_Chat.scrollToPosition(adapter.getItemCount() - 1);
+                            ToastUtil.showShort("发送成功");
+                            et_chat_message.setText("");
+                        }
+                    }.execute();
                 }
+
             }
+
         });
     }
 }
