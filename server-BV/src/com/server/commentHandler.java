bfc214b84e222b901214f4055f59e322227057fe
LarissaTan <85235372+LarissaTan@users.noreply.github.com>
2023-06-21 22:37:00 +0800

    just for backup

--- server-BV/src/com/server/commentHandler.java
+++ server-BV/src/com/server/commentHandler.java
@@ -1,8 +1,8 @@
 package com.server;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
+import com.example.bv_mart.bean.chatObject;
+
+import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 
@@ -14,11 +14,55 @@ public class commentHandler implements Runnable{
     private ObjectOutputStream writer;  // replace buffered writer
 
     public commentHandler(Socket socket){
-        
+        try{
+            this.socket = socket;
+            this.reader = new ObjectInputStream(socket.getInputStream());
+            this.writer = new ObjectOutputStream(socket.getOutputStream());
+            array.add(this);
+        }catch (IOException e){
+            closeEverything(socket,reader,writer);
+        } 
     }
 
-    @Override
+    @Override   // run on a separate thread
     public void run() {
+        chatObject msg;
 
+        while(socket.isConnected()){
+            try{
+                msg = (chatObject) reader.readObject();
+                boardcast(msg);
+            } catch (IOException | ClassNotFoundException e) {
+                closeEverything(socket,reader,writer);
+                break;
+            }
+        }
+
+    }
+
+    public void boardcast(chatObject msg){
+        for(commentHandler a: array){
+            try{
+                a.writer.writeObject(msg);
+                a.writer.flush();
+            } catch (IOException e) {
+                closeEverything(socket,reader,writer);
+            }
+        }
+    }
+
+    public void removeCommentHandler(){
+        array.remove(this);
+    }
+
+    public void closeEverything(Socket socket, ObjectInputStream reader, ObjectOutputStream writer){
+        removeCommentHandler();
+        try{
+            if(reader != null)  reader.close();
+            if(writer != null)  writer.close();
+            if(socket != null)  socket.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
